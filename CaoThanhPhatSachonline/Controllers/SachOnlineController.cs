a712e5eb570aa81f7edae50959448c6d8be65a53
phatcao285 <2124802010172@student.tdmu.edu.vn>
2023-09-19 10:25:44 +0700

    Add project files.

---
+++ CaoThanhPhatSachonline/Controllers/SachOnlineController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaoThanhPhatSachonline.Controllers
+{
+    public class SachOnlineController : Controller
+    {
+        // GET: SachOnline
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ChuDePartial()
+        {
+            return PartialView();
+        }
+        public ActionResult SachBanNhieuPartial()
+        {
+            return PartialView();
+        }
+        public ActionResult navPartial()
+        {
+            return PartialView();
+        }
+        public ActionResult SliderPartial()
+        {
+            return PartialView();
+        }
+        public ActionResult NhaXuatBanPartial()
+        {
+            return PartialView();
+        }
+        public ActionResult FooterPartial()
+        {
+            return PartialView();
+        }
+
+    }
+}
